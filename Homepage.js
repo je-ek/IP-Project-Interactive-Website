fb2fd5ef7f6f2a76380c39f0ea2633982bd9aeef
je-ek <s10222027@connect.np.edu.sg>
2022-02-19 23:10:22 +0800

    Logo click scroll to top of scrollable container + footer fleshed out + main body scrolling and custom scrollbar

--- Homepage.js
+++ Homepage.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-    document.querySelector("#loginBack").addEventListener("click", function(e){
+    document.querySelector("#signOut").addEventListener("click", function(e){
         e.preventDefault();
-        console.log("activate");
+        console.log("sign me out cheif");
         function redirect(){
             window.location.assign("Loginpage.html");
         }
         setTimeout(redirect, 1000);
     });
+    document.querySelector(".homeLogo").addEventListener("click",function(e){
+        e.preventDefault();
+        console.log("click");
+
+        document.querySelector(".scrollableContainer").scrollTop = 0;
+    })
 });
